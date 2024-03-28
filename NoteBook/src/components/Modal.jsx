fcd2f1391d6abd11d6664cf548056733e1f48042
alokkumarsingh07 <kalok8841@gmail.com>
2024-03-28 22:03:35 +0530

    NoteBook

--- NoteBook/src/components/Modal.jsx
+++ NoteBook/src/components/Modal.jsx
@@ -1,3 +1,48 @@
+// import React, { useState } from "react";
+// import { useNotebook } from "./NoteBookContext";
+
+// const Modal = ({ onClose }) => {
+//   const { addNote } = useNotebook();
+//   const [noteTitle, setNoteTitle] = useState("");
+//   const [noteContent, setNoteContent] = useState("");
+
+//   const handleAddNote = () => {
+//     addNote({
+//       id: Date.now(),
+//       title: noteTitle,
+//       content: noteContent,
+//     });
+//     onClose();
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         <label htmlFor="txt">Note Title: </label>
+//         <input
+//           id="txt"
+//           type="text"
+//           placeholder="Note Title"
+//           value={noteTitle}
+//           onChange={(e) => setNoteTitle(e.target.value)}
+//         />
+//         <br />
+//         <label htmlFor="cnt">Note Content: </label>
+//         <textarea
+//           id="cnt"
+//           placeholder="Note Content"
+//           value={noteContent}
+//           onChange={(e) => setNoteContent(e.target.value)}
+//         />
+//         <br />
+//         <button onClick={handleAddNote}>Add to Book</button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Modal;
+
 import React, { useState } from "react";
 import { useNotebook } from "./NoteBookContext";
 
@@ -16,28 +61,55 @@ const Modal = ({ onClose }) => {
   };
 
   return (
-    <div>
-      <div>
-        <label htmlFor="txt">Note Title: </label>
-        <input
-          id="txt"
-          type="text"
-          placeholder="Note Title"
-          value={noteTitle}
-          onChange={(e) => setNoteTitle(e.target.value)}
-        />
-        <br />
-        <label htmlFor="cnt">Note Content: </label>
-        <textarea
-          id="cnt"
-          placeholder="Note Content"
-          value={noteContent}
-          onChange={(e) => setNoteContent(e.target.value)}
-        />
-        <br />
-        <button onClick={handleAddNote}>Add to Book</button>
+    <>
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          background: "rgba(0, 0, 0, 0.5)",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        <div
+          style={{
+            background: "#111",
+            height: 150,
+            width: 240,
+            margin: "auto",
+            padding: "2%",
+            border: "2px solid #000",
+            borderRadius: "10px",
+            boxShadow: "2px solid black",
+          }}
+        >
+          <label htmlFor="txt">Note Title: </label>
+          <input
+            id="txt"
+            type="text"
+            placeholder="Note Title"
+            value={noteTitle}
+            onChange={(e) => setNoteTitle(e.target.value)}
+          />
+          <br />
+          <label htmlFor="cnt">Note Content: </label>
+          <textarea
+            id="cnt"
+            placeholder="Note Content"
+            value={noteContent}
+            onChange={(e) => setNoteContent(e.target.value)}
+          />
+          <br />
+          <button onClick={handleAddNote}>Add to Book</button>
+          <button onClick={onClose}>Close</button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
